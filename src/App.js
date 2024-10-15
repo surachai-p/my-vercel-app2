84fc560e75936d715d0465e43df48a938dce3983
surachai-p <surachai.pi@kmitl.ac.th>
2024-10-15 10:57:49 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+function App() {
+return (
+<div className="App">
+<header className="App-header">
+<h1>Welcome to My Vercel App</h1>
+<p> This is a simple React app deployed on Vercel. </p>
+<a className="App-link" href="https://vercel.com" target="_blank"
+rel="noopener noreferrer" > Learn about Vercel </a>
+</header>
+</div>
+);
+}
+export default App;
