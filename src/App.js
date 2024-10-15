da72f4d563a9370f962cae1042bbfb876a4a8f03
surachai-p <surachai.pi@kmitl.ac.th>
2024-10-15 11:40:52 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
 return (
 <div className="App">
 <header className="App-header">
-<h1>Welcome to My Vercel App</h1>
+<h1>Welcome to My Vercel App jkhkhjkhkjhkjhkjh</h1>
 <p> This is a simple React app deployed on Vercel. </p>
 <a className="App-link" href="https://vercel.com" target="_blank"
 rel="noopener noreferrer" > Learn about Vercel </a>
